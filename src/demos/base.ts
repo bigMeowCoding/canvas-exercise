80cc6aa2a47a321677b51f5fb779c9d5da368cce
zhouyijun <zhou495248579@hotmail.com>
2021-03-19 11:03:59 +0800

    星空颜色

--- src/demos/base.ts
+++ src/demos/base.ts
@@ -1,10 +1,37 @@
-import { drawArrow, drawRect, getCanvasBase } from "./utils";
+import { drawArrow, drawRect, drawStar, getCanvasBase } from "./utils";
+
+function drawStarSky(count: number) {
+  const { context, canvas } = getCanvasBase();
+  if (!context) {
+    return;
+  }
+  const skyStyle = context.createLinearGradient(0, 0, 0, canvas.height);
+  skyStyle.addColorStop(0, "black");
+  skyStyle.addColorStop(1.0, "#035");
+  context.fillStyle = skyStyle;
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < count; i++) {
+    const rot = 360 * Math.random(),
+      x = canvas.width * Math.random(),
+      y = canvas.height * 0.65 * Math.random(),
+      r = Math.random() * 10 + 10;
+    drawStar(
+      context,
+      {
+        x,
+        y,
+        rot,
+      },
+      { inner: r / 2, outer: r },
+      { fillStyle: "yellow" }
+    );
+  }
+}
 
 export function baseDraw() {
   const { context } = getCanvasBase();
   if (!context) {
     return;
   }
-  // drawArrow(context);
-  drawRect(context, 100, 100, 200, 200, 10, "#ddd", "red");
+  drawStarSky(200);
 }
--- src/demos/utils.ts
+++ src/demos/utils.ts
@@ -30,6 +30,41 @@ export function drawArrow(context: CanvasRenderingContext2D) {
   context.stroke();
 }
 
+export function drawStar(
+  context: CanvasRenderingContext2D,
+  pos: { x: number; y: number; rot: number },
+  radius: { inner: number; outer: number },
+  style: {
+    borderWidth?: number;
+    borderStyle?: string;
+    fillStyle?: string;
+  }
+) {
+  context.beginPath();
+  for (let i = 0; i < 5; i++) {
+    context.lineTo(
+      Math.cos(((18 - pos.rot + i * 72) * Math.PI) / 180) * radius.outer +
+        pos.x,
+      -1 * Math.sin(((18 - pos.rot + i * 72) * Math.PI) / 180) * radius.outer +
+        pos.y
+    );
+    context.lineTo(
+      Math.cos(((54 - pos.rot + i * 72) * Math.PI) / 180) * radius.inner +
+        pos.x,
+      -1 * Math.sin(((54 - pos.rot + i * 72) * Math.PI) / 180) * radius.inner +
+        pos.y
+    );
+  }
+  context.closePath();
+  context.strokeStyle = style.borderStyle ?? "";
+  context.lineWidth = style.borderWidth ?? 0;
+  context.lineJoin = "round";
+  context.fillStyle = style.fillStyle ?? "";
+
+  context.fill();
+  context.stroke();
+}
+
 export function drawRect(
   context: CanvasRenderingContext2D,
   x: number,
@@ -40,17 +75,10 @@ export function drawRect(
   borderColor: string,
   fillColor: string
 ) {
-  context.beginPath();
-  context.moveTo(x, y);
-  context.lineTo(x + width, y);
-  context.lineTo(x + width, y + height);
-  context.lineTo(x, y + height);
-  context.closePath();
-
   context.strokeStyle = borderColor;
   context.lineWidth = borderWidth;
   context.fillStyle = fillColor;
-
-  context.fill();
-  context.stroke();
+  // 不需要beginPath内部帮做了
+  context.fillRect(x, y, width, height);
+  context.strokeRect(x, y, width, height);
 }
