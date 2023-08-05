b97dfbe68ce2682aeff50ff67d109b874714c46f
zhouyijun <zhou495248579@hotmail.com>
2023-08-05 18:02:54 +0800

    feat:贝塞尔曲线例子

--- src/case5.js
+++ src/case5.js
@@ -1,15 +1,38 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+/**
+ * @description 贝塞尔曲线
+ */
 
-// Quadratic Bézier curve
-ctx.beginPath();
-ctx.moveTo(50, 20);
-ctx.lineTo(150, 20);
-ctx.lineTo(150, 120);
-ctx.lineTo(50, 120);
-ctx.closePath();
-ctx.strokeStyle = "red";
-ctx.fillStyle = "yellow";
-// ctx.strokeRect(50,20, 100, 100);
-// ctx.strokeRect(50,20, 100, 100);
-ctx.fill();
+
+function drawHeart(pos, option) {
+  const { controlPoint1, controlPoint2 } = option;
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  ctx.beginPath();
+  ctx.moveTo(pos.beginX, pos.beginY);
+  ctx.bezierCurveTo(
+    controlPoint1.x,
+    controlPoint1.y,
+    controlPoint2.x,
+    controlPoint2.y,
+    pos.endX,
+    pos.endY,
+  );
+  ctx.moveTo(pos.beginX, pos.beginY);
+  ctx.bezierCurveTo(
+    pos.beginX + (pos.beginX - controlPoint1.x),
+    controlPoint1.y,
+    pos.beginX + (pos.beginX - controlPoint2.x),
+    controlPoint2.y,
+    pos.endX,
+    pos.endY,
+  );
+  ctx.stroke();
+  ctx.fillStyle = "red";
+  ctx.fill();
+}
+
+drawHeart(
+  { beginX: 144, beginY: 77, endX: 144, endY: 157 },
+  { controlPoint1: { x: 60, y: 26 }, controlPoint2: { x: 71, y: 116 } },
+);
